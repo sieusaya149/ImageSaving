5c4d3401daf7b8f2a96948b48e52f17755f075f3
viethung <viethunghoang149@gmail.com>
2022-01-11 22:25:27 +0700

    change view function of Login and logout
    -- before wrong behaviors

--- pictureSaving/authenthicating/urls.py
+++ pictureSaving/authenthicating/urls.py
@@ -3,8 +3,8 @@ from authenthicating import views
 from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.home, name="home"),
-    path('login/', views.CustomLoginView.as_view(), name="login"),
-    path('logout/', views.logout, name="logout"),
+    path('login/', views.Login, name="login"),
+    path('logout/', views.Logout, name="logout"),
     path('register/', views.Register, name="register"),
     path('forget-password/', views.forgetPassPage, name="forget-password"),
     path('change-password/<token>/' , views.ChangePassword , name="change-password"),
